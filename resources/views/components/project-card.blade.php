53141455ca472ceeda6656e8e8e3f75b5f45999b
kafyar2022 <kafyar2022@gmail.com>
2022-07-26 17:54:23 +0500

    startups page form

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -3,5 +3,5 @@
 <figure class="project-card">
   <figcaption class="project-card__title">{{ $project->title }}</figcaption>
   <img class="project-card__logo" src="{{ asset('files/projects/' . $project->logo) }}" width="165" height="112" alt="{{ $project->title }}">
-  <a class="project-card__link" href="{{ $project->url }}">{{ $project->url }}</a>
+  <a class="project-card__link" target="_blank" href="{{ $project->url }}">{{ $project->url }}</a>
 </figure>
