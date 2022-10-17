95b8144f40b1c28ac12afeeb44bbb99eb14d289f
kafyar2022 <kafyar2022@gmail.com>
2022-10-17 16:12:25 +0500

    l;ksdhsdfgsgf

--- resources/views/pages/main/index.blade.php
+++ resources/views/pages/main/index.blade.php
@@ -65,7 +65,7 @@
       <section
         class="main-page__grid-item"
         style="background-image: url('img/main-page-management.jpg')">
-        <div class="content" style="margin-bottom: 40">
+        <div class="content" style="margin-bottom: 40px">
           <h3>Наш менеджмент</h3>
           <p>В нас сочетаются инновации и традиции. Мы знак истинного качества, символ верности
             и преданности своему делу, своему обществу и стране.</p>
